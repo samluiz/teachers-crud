2554754158d6fb1daefc1002b8b5c9c3638f8643
samluiz <samuelluizrl@outlook.com>
2023-01-18 00:31:29 -0300

    refactoring, creating SQL script

--- src/main/java/com/saurs/teacherscrud/models/enums/Subject.java
+++ src/main/java/com/saurs/teacherscrud/models/enums/Subject.java
@@ -2,14 +2,14 @@ package com.saurs.teacherscrud.models.enums;
 
 // Enum de disciplinas que o usuário vai poder escolher na combobox
 public enum Subject {
-    MATHEMATICS,
-    PORTUGUESE,
-    PHYSICS,
-    BIOLOGY,
-    CHEMISTRY,
-    GEOGRAPHY,
-    HISTORY,
-    PHILOSOPHY,
-    SOCIOLOGY,
-    OTHER
+    MATEMATICA,
+    PORTUGUES,
+    FISICA,
+    BIOLOGIA,
+    QUIMICA,
+    GEOGRAFIA,
+    HISTORIA,
+    FILOSOFIA,
+    SOCIOLOGIA,
+    OUTRA
 }
